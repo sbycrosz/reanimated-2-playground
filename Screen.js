5c18660cb126b13df29f07d5f68cf4d5279eddd6
Sam Aryasa <sambya@aryasa.net>
2021-03-24 17:13:19 +0800

    Add sample svg animation

--- Screen.js
+++ Screen.js
@@ -1,42 +1,76 @@
+/* eslint-disable react-native/no-inline-styles */
+
+import * as Svg from 'react-native-svg';
+
 import Animated, {
+  Easing,
+  useAnimatedProps,
   useSharedValue,
   withTiming,
-  useAnimatedStyle,
-  Easing,
 } from 'react-native-reanimated';
-import {View, Button} from 'react-native';
-import React from 'react';
+import {Button, View} from 'react-native';
+
+import React, {useState} from 'react';
+// import { interpolatePath } from 'd3-interpolate-path';
+
+function interpolatePath(path1, path) {
+  'worklet';
+  return () => {
+    'worklet';
+    return path1;
+  };
+}
+
+const PATH1 = 'M0,300 L100,100 L200,200 L300,100 L400,140';
+const PATH2 = 'M0,300 L100,150 L200,100 L300,200 L400,240';
+
+const interpolator = interpolatePath(PATH1, PATH2);
+
+const AnimatedSvgPath = Animated.createAnimatedComponent(Svg.Path);
 
 export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
+  const [progressNonAnimated, setProgressNonAnimated] = useState(0);
+  const progress = useSharedValue(0);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
+  const animatedProps = useAnimatedProps(() => {
+    // I think this will be run on RN thread, not sure how to move it to UI thread
+    const path = interpolator(progress.value);
+    return {d: path};
   });
 
   return (
-    <View
-      style={{
-        flex: 1,
-        flexDirection: 'column',
-      }}>
-      <Animated.View
-        style={[
-          {width: 100, height: 80, backgroundColor: 'black', margin: 30},
-          style,
-        ]}
-      />
+    <View style={{flex: 1, alignItems: 'center'}}>
+      <Svg.Svg style={{borderWidth: 1}} height={300} width={400}>
+        <AnimatedSvgPath
+          animatedProps={animatedProps}
+          strokeWidth={1}
+          stroke={'#000'}
+        />
+      </Svg.Svg>
+
       <Button
-        title="toggle"
+        title="toggle-animated"
         onPress={() => {
-          randomWidth.value = Math.random() * 350;
+          progress.value = withTiming(progress.value ? 0 : 1, {
+            duration: 1000,
+            easing: Easing.inOut(Easing.cubic),
+          });
+        }}
+      />
+
+      <Svg.Svg style={{borderWidth: 1}} height={300} width={400}>
+        <Svg.Path
+          d={interpolator(progressNonAnimated)}
+          animatedProps={animatedProps}
+          strokeWidth={1}
+          stroke={'#000'}
+        />
+      </Svg.Svg>
+
+      <Button
+        title="toggle-non-animated"
+        onPress={() => {
+          setProgressNonAnimated(progressNonAnimated ? 0 : 1);
         }}
       />
     </View>
